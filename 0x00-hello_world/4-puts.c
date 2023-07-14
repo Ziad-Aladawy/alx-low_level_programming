8f980f01546e21588e6ff444595dd8aea6b992d8
Ziad-Aladawy <you@example.com>
2023-07-13 19:45:43 -0700

    auto

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(){
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+/**
+*main - entery poit
+*return: always 0 (success)
+*/
+int main(void){
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
