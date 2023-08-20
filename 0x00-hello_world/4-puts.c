ccf7691ece59137b822e024e7432d7c884fdf65d
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-08-20 18:33:06 +0300

    auto

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - prints a sentence using puts()
+ * Return: always returns 0 - success
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
