dd9169350529f9ac6cab3f361fa9c3a300c089b4
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-08-20 18:35:50 +0300

    auto

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - prints a string using printf
+ * Return: always returns 0 - success
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
