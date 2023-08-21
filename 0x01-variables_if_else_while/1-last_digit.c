fc1d0b8d9301e25ae80df17ed680fd2098850a5d
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-08-21 12:46:06 +0300

    auto

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int n = rand() - RAND_MAX / 2;
 
-	int m = n % 10;
 
 	srand(time(0));
+	int m = n % 10;
 	if (n > 5)
 		printf("Last digit of %d is %d and is greater than 5", n, m);
 	else if (n == 0)
---
+++ 0x01-variables_if_else_while/test/hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - description
+ *
+ * Return: returns 0 (success)
+ */
+
+int main(void)
+{
+	printf("%ld\n", __STDC_VERSION__);
+}
+
