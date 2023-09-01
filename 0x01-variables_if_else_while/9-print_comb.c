ec601595c78652de8a1f370a1ebed6e7e343a8cb
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-09-01 22:42:59 +0300

    auto

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,16 +12,16 @@ int main(void)
 {
 	int i;
 	
-    for (i = '0'; i <= '9'; i++)
-    {
-        putchar(i);
-        if (i == '9')
-        {
-            putchar('\n');
-            return (0);
-        }
-        putchar(',');
-        putchar(' ');
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i == '9')
+		{
+			putchar('\n');
+			return (0);
+		}
+		putchar(',');
+		putchar(' ');
     }
     return (0);
 }
