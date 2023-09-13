fe974f3034e0b3a23094724a1b8f768495f85449
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-09-13 22:51:46 +0300

    auto

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,11 +12,15 @@ void print_alphabet_x10(void)
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
-			if (ch == 'z' && cnt <= 10)
+			if (ch == 'z')
 			{
 				_putchar('\n');
 				cnt++;
+			}
+			if (cnt < 10)
+			{
 				ch = 96;
 			}
+
 		}
 }
