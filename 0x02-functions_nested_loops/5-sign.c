eb67bced040351aeb227cea9b69b29aed8e31782
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-09-14 19:03:17 +0300

    auto

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+* print_sign - Function
+* Description: it shows the sign of the number given
+* @n: the number to be checked
+* Return: returns the 1 if positive -1 if negative and 0 if 0
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (0);
+}
