7d21478ba39d9129601b69e48566172160df3bfe
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-09-14 19:06:57 +0300

    auto

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,9 +10,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
+		_putchar('+1');
 		return (1);
-	else if (n < 0)
-		return (-1);
+	}
+	if else (n < 0)
+	{
+		_putchar('-1');
+		return (1);
+	}
 	else
-		return (0);
+	{
+		_putchar('00');
+		return (1);
+	}
 }
