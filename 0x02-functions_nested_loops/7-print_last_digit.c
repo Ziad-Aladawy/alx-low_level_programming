f3f96cf548bebe92b1d428ede2346576ed90606f
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-09-14 19:22:25 +0300

    auto

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* print_last_digit - Function
+* Description: gets the last digit in a number
+* @n: the number to be checked
+* Return: @n
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
