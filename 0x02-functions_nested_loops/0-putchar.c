80c41bd5121729f1baefa0595398fd792aaad499
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-09-14 19:11:11 +0300

    auto

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,15 @@
 */
 int main(void)
 {
-	// char *string = "_putchar";
+	char *string = "_putchar";
 
-	// while (*string != '\0')
-	// {
-	// 	_putchar(*string);
-	// 	string++;
-	// }
-	// putchar('\n');
+	while (*string != '\0')
+	{
+		_putchar(*string);
+		string++;
+	}
+	putchar('\n');
 
-	print_alphabet_x10();
 	return (0);
 }
 
