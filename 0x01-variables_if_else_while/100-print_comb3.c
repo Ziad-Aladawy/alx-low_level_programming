e8ad450fe66db9130da57e9d90a6a86ee3840e5a
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-11-23 09:03:21 +0200

    23/11/2023

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void combo2(void);
+typedef struct max Struct;
 /**
  * main - prints all possible combinations of 2 digits
  * Return: always returns 0 - success
@@ -8,26 +9,64 @@
 
 int main(void)
 {
-	int i, j, k;
+	combo2();
+	return (0);
+}
+/**
+ * struct max - Short description
+ * @maxi: First member
+ * @maxj: Second member
+ *
+ * Description: Longer description
+ */
+struct max
+{
+	int maxi, maxj;
+};
+/**
+ * maxCombo - get the largest combination possible
+ * @maxi: maximum i
+ * @maxj: maximum j
+ * Return: the max number
+*/
+Struct maxCombo(void)
+{
+	Struct max;
+	int i, j;
 
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = i + 1; j <= '9'; j++)
 		{
-			for (k = j + 1; k <= '9'; k++)
-			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (i == '7' && j == '8' && k == '9')
-				{
-					putchar('\n');
-					return (0);
-				}
-				putchar(',');
-				putchar(' ');
-			}
+			max.maxi = i;
+			max.maxj = j;
 		}
 	}
-	return (0);
+
+	return (max);
+}
+/**
+ * combo2 - prints all 2 digit combinations
+ * Return: Void
+*/
+void combo2(void)
+{
+	int i, j;
+	Struct res = maxCombo();
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == res.maxi && j == res.maxj)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 }
