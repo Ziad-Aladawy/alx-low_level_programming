6b4ac53a8dc54abd2927ede97e1a703f26d76254
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-11-23 08:03:12 +0200

    23/11/2023

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,16 +8,21 @@
 
 int main(void)
 {
+	putAlpha();
+	return (0);
+}
+
+/**
+ * putAlpha - the function that prints the task
+ * Return: Void
+*/
+void putAlpha(void)
+{
 	int i;
 
-	for (i = 97; i <= 123; i++)
+	for (i = 97; i <= 122; i++)
 	{
-		if (i == 123)
-		{
-			putchar(10);
-			return (0);
-		}
 		putchar(i);
 	}
-	return (0);
+	putchar('\n');
 }
