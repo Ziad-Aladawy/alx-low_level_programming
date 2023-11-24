1801727b8faebaacc555501cc08f50fc3c416f25
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-11-24 03:09:57 +0200

    24/11/2023

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * length - get the length of bit of the number
+ * @n: the number
+ * Return: length
+*/
+int length(unsigned long int n)
+{
+	int l = 0;
+
+	while (n > 0)
+	{
+		l++;
+		n >>= 1;
+	}
+	return (l);
+}
+
+/**
+* print_binary - prints binary from decimal
+* @n: number to be converted
+* Return: nothing
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long long i;
+
+	for (i = 1 << length(n); i > 0; i >> 1)
+	{
+		if (n & i)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
