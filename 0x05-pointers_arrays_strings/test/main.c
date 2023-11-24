ae072b55c4aaff4a2f79bd3008d4e1267a53752b
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-11-24 17:26:55 +0200

    auto

---
+++ 0x05-pointers_arrays_strings/test/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * main - description
+ *
+ * Return: returns 0 (success)
+ */
+
+int main(void)
+{
+	char *c = "hello";
+
+	print_rev(c);
+}
+
